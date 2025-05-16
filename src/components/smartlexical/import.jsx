e3168e52dbec55074c95695239f5a37437522ed2
efsaa <143024930+efsaa@users.noreply.github.com>
2025-05-17 00:13:36 +0300

    Create import.jsx

---
+++ src/components/smartlexical/import.jsx
@@ -0,0 +1,10 @@
+import SmartLexicalEditor from "@/components/SmartLexicalEditor";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Keşif Küresi Editor</h1>
+      <SmartLexicalEditor />
+    </div>
+  );
+}
